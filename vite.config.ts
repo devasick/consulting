4fc2bdf289f865591b690c8ecc0c417963ae965e
Asick <ashiq.it@gmail.com>
2026-02-28 22:23:48 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -28,9 +28,10 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist"), 
+    outDir: "dist",
     emptyOutDir: true,
   },
+  base: "/",
   server: {
     fs: {
       strict: true,
